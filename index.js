a5064ccfa983316f89f9f614f9ca9c79cff350b3
ShivaPrime82 <jeremy.clair.82@gmail.com>
2020-03-20 15:07:07 +0100

    add style for image , add json , add many many lign of code

--- index.js
+++ index.js
@@ -21,3 +21,19 @@ app.get('/api/user', (req, res) => {
         }),    
     });
 });
+
+const profile = document.getElementById('main-content');
+const response = await fetch ('/api/user');
+const img = profile.getElementsByClassName('img-profile');
+img.innerHtml = img.img
+
+const json = await response.json();
+const name = profile.getElementsByClassName('name')[0];
+const verified = profile.getElementsByClassName('boolean')[0];
+const birthdate = profile.getElementsByClassName('birthday')[0];
+const friends = profile.getElementsByClassName('array')[0];
+
+name.innerHTML = json.name;
+verified.innerHTML = json.verified;
+title.birthdate = json.birthdate;
+title.friends = json.friends;
