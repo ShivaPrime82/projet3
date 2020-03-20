1024cd60444c7ea245200fddd67d9f2eba54a169
ShivaPrime82 <jeremy.clair.82@gmail.com>
2020-03-20 15:42:22 +0100

    update json , add image link and comment

--- index.js
+++ index.js
@@ -11,29 +11,26 @@ app.use(express.static("./public"));
 
 app.get('/api/user', (req, res) => {
     res.send({
-        "name": Jérémy,
-        "verified": true,
-        "birthdate": 04/11/1992,
-        "friends": ({
-            "1": Moi,
-            "2": MoiEncore,
-            "3": EncoreMoi
-        }),    
+        name: "Jérémy",
+        verified: "true",
+        birthdate: "04/11/1992",
+        friends: ([ "Moi", " MoiEncore", " EncoreMoi" ]
+            ),    
     });
 });
 
-const profile = document.getElementById('main-content');
-const response = await fetch ('/api/user');
-const img = profile.getElementsByClassName('img-profile');
-img.innerHtml = img.img
+// const profile = document.getElementById('main-content');
 
-const json = await response.json();
-const name = profile.getElementsByClassName('name')[0];
-const verified = profile.getElementsByClassName('boolean')[0];
-const birthdate = profile.getElementsByClassName('birthday')[0];
-const friends = profile.getElementsByClassName('array')[0];
+// const response = fetch ('/api/user'); //await fetch ('/api/user');
 
-name.innerHTML = json.name;
-verified.innerHTML = json.verified;
-title.birthdate = json.birthdate;
-title.friends = json.friends;
+// const json = response.json(); //await response.json();
+
+// const name = profile.getElementsByClassName('name')[0];
+// const verified = profile.getElementsByClassName('boolean')[0];
+// const birthdate = profile.getElementsByClassName('birthday')[0];
+// const friends = profile.getElementsByClassName('array')[0];
+
+// name.innerHTML = json.name;
+// verified.innerHTML = json.verified;
+// birthdate.innerHTML = json.birthdate;
+// friends.innerHTML = json.friends;
